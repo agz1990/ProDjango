3187e2ca5f1f990558b21832a350140c2db95e6c
agz1990 <mrji1990@gmail.com>
2015-11-26 15:55:46 +0800

    1。 weblog 使用generic类进行通用化处理

--- ProDjango/urls.py
+++ ProDjango/urls.py
@@ -16,16 +16,28 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
+from django.views import generic
+from coltrane.models import Entry
+
+entry_info_dict = {
+    'date_field': 'pub_date',
+    'queryset': Entry.objects.all(),
+    'month_format': '%m',
+}
 
 urlpatterns = [
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tinymce/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.TINY_MCE_ROOT}),
-    url(r'weblog/$', 'coltrane.views.entries_index'),
+
+    url(r'weblog/$', generic.ArchiveIndexView.as_view(queryset=Entry.objects.all(), date_field='pub_date')),
+    url(r'weblog/(?P<year>\d{4})/(?P<month>\d{2})/$',
+        generic.MonthArchiveView.as_view(**entry_info_dict)),
     url(r'weblog/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
- 'coltrane.views.entry_detail'),
+        generic.DateDetailView.as_view(**entry_info_dict)),
+
     url(r'^search/$', 'search.views.search'),
-    url(r'', include('django.contrib.flatpages.urls')),
+    # url(r'', include('django.contrib.flatpages.urls')),
 
 
 
--- coltrane/views.py
+++ coltrane/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.views import generic
 from models import Entry
 
 # Create your views here.
 def entries_index(request):
 
-    return render_to_response('coltrane/entry_index.html',
+    return render_to_response('coltrane/entry_archive.html',
                               {'entry_list': Entry.objects.all()})
 
 def entry_detail(request, year, month, day, slug):
@@ -13,7 +14,7 @@ def entry_detail(request, year, month, day, slug):
     import time
     date_stamp = time.strptime(year+month+day, "%Y%m%d")
     pub_data = datetime.date(*date_stamp[:3])
-    
+
     entry = get_object_or_404(Entry,
                               pub_date__year=pub_data.year,
                               pub_date__month=pub_data.month,
